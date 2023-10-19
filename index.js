a4da350c5586be11cd48db282cba9edd09febd5e
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2023-10-20 02:47:32 +0600

    Database created and product collection created + single product insert created+ brand wise all data dfind method created

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const brands = require('./brands.json');
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const brands = require("./brands.json");
+const app = express();
+const port = process.env.PORT || 3000;
 
 //middle ware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 console.log(brands);
 
@@ -22,13 +22,41 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-  
     await client.connect();
+    //DATABASE AND COLLECTION STARTS
+
+    const database = client.db("wheelsWorldDB");
+    const productsCollection = database.collection("products");
+
+    //DATABASE AND COLLECTION ENDS
+    //CREATE PRODUCTS
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      console.log("New Product", product);
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
+    //READ ALL PRODUCTS
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get Brandname wise data
+    app.get('/products/:brandName',async(req,res)=>{
+      let brand = req.params.brandName;
+      let query = { brandName: brand };
+      const cursor =  productsCollection.find(query)
+      const result = await cursor.toArray()
+      console.log("Requested data: ",brand);
+      res.send(result)
+    })
 
 
 
@@ -39,13 +67,11 @@ async function run() {
 
 
 
-
-
-
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -53,16 +79,14 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 // server backend
 
-app.get("/", (req,res)=>{
-    res.send("Automotiv data v101 server is on and running...")
-})
-app.get("/brands", (req,res)=>{
-    res.send(brands)
-})
-app.listen(port, ()=>{
-   console.log(`Your server is running on PORT : ${port}`);
-})
+app.get("/", (req, res) => {
+  res.send("Automotiv data v101 server is on and running...");
+});
+app.get("/brands", (req, res) => {
+  res.send(brands);
+});
+app.listen(port, () => {
+  console.log(`Your server is running on PORT : ${port}`);
+});
